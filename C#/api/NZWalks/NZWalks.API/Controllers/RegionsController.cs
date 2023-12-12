a0ab954e468668eacd9032290e3f6114a3b9a764
Bevanwhite <jefferybevan@gmail.com>
2023-12-12 13:35:49 +0530

    added the NZWalks GetAll() and GetById() using DTOs.

---
+++ C#/api/NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegionsController : ControllerBase
+	{
+		private readonly NZWalksDbContext dbContext;
+		public RegionsController(NZWalksDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+
+		// Get All Regions
+		// Get: https://localhost:portnumber/api/regions
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			//var regions = new List<Region>
+			//{
+			//	new Region
+			//	{
+			//		Id = Guid.NewGuid(),
+			//		Name = "Auckland Region",
+			//		Code = "AKL",
+			//		RegionImageUrl = "https://unsplash.com/photos/a-boardwalk-leading-to-a-steaming-geyser-at-sunset-QgiYyDUcWi0"
+			//	},
+			//	new Region
+			//	{
+			//		Id = Guid.NewGuid(),
+			//		Name = "Wellington Region",
+			//		Code = "WLG",
+			//		RegionImageUrl = "https://unsplash.com/photos/a-bride-and-groom-holding-a-bouquet-of-flowers-3QupYkJJEAI"
+			//	}
+			//};
+			// get data from Database - Domain models
+			var regions = dbContext.Regions.ToList();
+
+			// Map Domain Models to DTOs
+			var regionsDto = new List<RegionDto>();
+			foreach (var region in regions)
+			{
+				regionsDto.Add(new RegionDto()
+				{
+					Id = region.Id,
+					Code = region.Code,
+					Name = region.Name,
+					RegionImageUrl = region.RegionImageUrl
+				});
+			}
+
+			// return DTOs
+			return Ok(regionsDto);
+		}
+
+		// Get Single Region (Get Region by Id)
+		// Get: https://localhost:portnumber/api/regions/{id}
+		[HttpGet]
+		[Route("{id:Guid}")]
+		public IActionResult GetById([FromRoute] Guid id)
+		{
+			// var region = dbContext.Regions.Find(id);
+			// another way to get the Region by Id
+			var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+			if (region == null)
+			{
+				return NotFound();
+			}
+
+			var regionDto = new RegionDto
+			{
+				Id = region.Id,
+				Code = region.Code,
+				Name = region.Name,
+				RegionImageUrl = region.RegionImageUrl
+			};
+
+			// return the DTO
+			return Ok(regionDto);
+		}
+
+
+	}
+}
--- C#/api/NZWalks/NZWalks.API/readme.md
+++ C#/api/NZWalks/NZWalks.API/readme.md
@@ -1,4 +1,5 @@
-﻿1. Solution can have multiple projcets
+﻿Programme structure 
+1. Solution can have multiple projcets
 2. if you double click on `NZWalks.API`
 	* this is a `*.csproj` file
 	* we can see some properties in this file
@@ -14,3 +15,45 @@
 	* this file is running first
 	* you can add dependencies to our application
 	* you can do dependency injection 
+
+
+what is Rest?
+	* style of architecture for building web services
+	* rest is set of principles that how web services should be design and interact with each other
+	* Rest is based on resources.
+	* server should not state any client state between this request.
+	* client should provide all the necessary things in each request.
+	* it should be able handle multiple request because no need to state the client state
+
+why .net core? 
+	* Http verbs
+	* Routing
+	* Model binding
+	* content negotiation
+	* Response types
+
+what are http verbs?
+	* http verbs defines the type actions that can be performed on a resource identified by a URI
+	* GET		-	
+	* POST		-
+	* PUT		-
+	* DELETE	-
+	* PATCH		-
+	* OPTIONS	-
+
+
+how add the database and the migration
+	* `Add-Migration "Initial Migration"`
+	* `Update-Database`
+
+what are DTO ? 
+	* DTO stands for Data Transfer Objects
+	* used to transfer data between different layers
+	* Typically contain a subsetof the properties in the domain model
+	* For example transferring data over a network
+
+Advantages of DTOs
+	* Separation of concerns
+	* performance
+	* security
+	* Versioning
