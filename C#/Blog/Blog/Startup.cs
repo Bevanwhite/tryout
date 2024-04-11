3bd53c501f25e043293a143510c090ebe83eadf7
Bevanwhite <jefferybevan@gmail.com>
2024-04-11 18:32:42 +0530

    creating blog site form dotnet 2.1

---
+++ C#/Blog/Blog/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog
+{
+	public class Startup
+	{
+		private readonly IConfiguration _config;
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+		public Startup(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
+
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+
+			app.UseMvcWithDefaultRoute();
+			//app.Run(async (context) =>
+			//{
+			//	await context.Response.WriteAsync("Hello World!");
+			//});
+		}
+	}
+}
