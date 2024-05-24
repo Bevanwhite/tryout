5fe1e2fce03c6f5f6f667c2938c4ada94198b642
Bevanwhite <jefferybevan@gmail.com>
2024-05-24 20:02:30 +0530

    remove the tryout html project and python project

---
+++ C#/HelloName/StudentApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StudentApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			int stuId = 1000;
+			string studentId = "1000";
+
+			stuId = stuId + 1;
+
+			studentId = studentId + 1;
+
+			Console.WriteLine($"stuId = {stuId}, StudentId = {studentId}");
+
+			Console.ReadKey();
+			*/
+			byte stuId = 0;
+
+			Console.WriteLine("Please enter an Id for this student");
+			stuId = Convert.ToByte(Console.ReadLine());
+			Console.WriteLine($"stuId: {stuId}");
+			Console.ReadKey();
+		}
+	}
+}
